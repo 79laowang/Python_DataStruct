a7217a746f46cf19728c5e7c27aa2125340da29b
79laowang <edward.wang@163.com>
2019-10-31 23:20:23 +0800

    add comments for bubble sort

--- bubble_sort.py
+++ bubble_sort.py
@@ -9,9 +9,27 @@
 # Licence:     <your licence>
 #-------------------------------------------------------------------------------
 #
+#demonstration:
+#alist -> [53,34,65,27,14]
+#1st round of inter loop do:
+#   -> [34,53,65,27,14]
+#     -> [34,53,27,65,14]
+#       -> [34,53,27,14,65]
+#   max - > 65
+#2rd round of inter loop do:[34,53,27,14 |65]
+#   -> [34,27,53,14 |65]
+#     -> [34,27,14,53 |65]
+#   max - > 53 
+#3rd round of inter loop do:[34,27,14 |53,65]
+#   -> [27,34,14 |53,65]
+#     -> [27,14,34 |53,65]
+#   max - > 34 
+#...
 # Worst time complexity: O(n^2)
 # Optimal time complexity: O(n)
 #
+
+from __future__ import print_function
 
 def bubble_sort(alist):
     """Bubble Sort unstable"""
@@ -29,9 +47,9 @@ def bubble_sort(alist):
 
 def main():
     list1 = [23,34,65,77,84,37,98,12,9,34]
-    print list1
+    print(list1)
     bubble_sort(list1)
-    print list1
+    print(list1)
 
 if __name__ == '__main__':
     main()
