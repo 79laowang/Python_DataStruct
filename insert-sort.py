f347156d2cb0c7c76bb6b347be4306eac6ed408c
79laowang <edward.wang@163.com>
2019-11-06 23:08:14 +0800

    optimize insert sort

--- insert-sort.py
+++ insert-sort.py
@@ -11,23 +11,22 @@
 # Licence:     <your licence>
 #-------------------------------------------------------------------------------
 
-##demonstration:
-##alist -> [23,34,65,77,84,37,98,12,9,34]
-##rounds    [23,   34,65,77,84,37,98,12,9,34]
-##          [23,34,   65,77,84,37,98,12,9,34]
-##          [23,34,65,   77,84,37,98,12,9,34]
-##          [23,34,65,77   ,84,37,98,12,9,34]
-##          [23,34,65,77,84,   37,98,12,9,34]
-##          [23,34,37,65,77,84,   98,12,9,34]
-##          [23,34,37,65,77,84,98   ,12,9,34]
-##          [12,23,34,37,65,77,84,98,   9,34]
-##          [9,12,23,34,37,65,77,84,98,   34]
-##          [9,12,23,34,34,37,65,77,84,98]
+#demonstration:
+#alist -> [23,34,65,77,84,37,98,12,9,34]
+#rounds    [23,   34,65,77,84,37,98,12,9,34]
+#          [23,34,   65,77,84,37,98,12,9,34]
+#          [23,34,65,   77,84,37,98,12,9,34]
+#          [23,34,65,77   ,84,37,98,12,9,34]
+#          [23,34,65,77,84,   37,98,12,9,34]
+#          [23,34,37,65,77,84,   98,12,9,34]
+#          [23,34,37,65,77,84,98   ,12,9,34]
+#          [12,23,34,37,65,77,84,98,   9,34]
+#          [9,12,23,34,37,65,77,84,98,   34]
+#          [9,12,23,34,34,37,65,77,84,98]
 #
 # Worst time complexity: O(n^2)
 # Optimal time complexity: O(n)
 #
-
 
 from __future__ import print_function
 
@@ -38,16 +37,12 @@ def insert_sort(alist):
     for j in range(1, n):
         i = j
         # Variable i indicates the left list sorting times
-        while i > 0:
-            if alist[i] < alist[i-1]:
-                # Exchange the minmum from tail list to the front list
-                alist[i],alist[i-1] = alist[i-1],alist[i]
-                i -= 1
+        while i > 0 and alist[i] < alist[i-1]:
+            # Exchange the minmum from tail list to the front list
+            alist[i],alist[i-1] = alist[i-1],alist[i]
+            i -= 1
             # Selected item from right unsort list inserted the right postion
             # in left sorting list, quit while loop
-            else:
-                break
-
 
 def main():
     list1 = [23,34,65,77,84,37,98,12,9,34]
