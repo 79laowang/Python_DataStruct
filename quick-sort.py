1b78a520ee10746ffae949b7869fb465bb8b2b73
79laowang <edward.wang@163.com>
2019-11-08 14:05:18 +0800

    add pythonic style quick sort

--- quick-sort.py
+++ quick-sort.py
@@ -2,8 +2,13 @@
 # -*- coding:utf-8 -*-
 #-------------------------------------------------------------------------------
 # Name:        Quick Sorting
-# Purpose:
-#
+# Purpose:     快速排序 
+#  稳定性：快排是一种不稳定排序，比如基准值的前后都存在与基准值相同的元素，那么相同值就会被放在一边，这样就打乱了之前的相对顺序
+#  比较性：因为排序时元素之间需要比较，所以是比较排序
+#  时间复杂度：快排的时间复杂度为O(nlogn)
+#  空间复杂度：排序时需要另外申请空间，并且随着数列规模增大而增大，其复杂度为：O(nlogn)
+#  缺点: 对于小规模的数据集性能不是很好。
+#  Solution: 当分区的规模达到一定小时，便停止快速排序算法，而是改用插入排序，因为插入排序在对基本有序的数据集排序有着接近线性的复杂度
 # Author:      kkewang
 #
 # Created:     01/06/2019
@@ -83,11 +88,31 @@ def quick_sort(alist, first, last):
     # quick sort right list
     quick_sort(alist,low + 1, last)
 
+def quick_sort_pythonic(arr):
+    """快速排序 pythonic"""
+    n = len(arr)
+    if n < 2:
+        return arr
+
+    mid = arr[n // 2]
+    left, right = [], []
+    arr.remove(mid)
+    for item in arr:
+        if item >= mid:
+            right.append(item)
+        else:
+            left.append(item)
+    return quick_sort_pythonic(left) + [mid] + quick_sort_pythonic(right)
+
+
 def main():
     list1 = [23,34,65,77,84,37,98,12,9,34]
     print(list1)
     quick_sort(list1, 0, len(list1)-1)
     print(list1)
+    print("Pythonic quick sorting ...")
+    list1 = [23,34,65,77,84,37,98,12,9,34]
+    print(quick_sort_pythonic(list1))
 
 if __name__ == '__main__':
     main()
